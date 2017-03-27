a0a68fd14bda4a4fb8dd319de9f0bb79bfdf3679
Andrew Oakley <anoakley@microsoft.com>
2017-03-27 16:42:31 -0700

    Pre-rename

--- spikes/nodejs-spike/templates/ValidationMessages.js
+++ spikes/nodejs-spike/templates/ValidationMessages.js
@@ -3,9 +3,13 @@
 //export { StringCannotBeNullUndefinedEmptyOrOnlyWhitespace };
 //exports.StringCannotBeNullUndefinedEmptyOrOnlyWhitespace = StringCannotBeNullUndefinedEmptyOrOnlyWhitespace;
 exports.StringCannotBeNullUndefinedEmptyOrOnlyWhitespace = 'Field cannot be null, undefined, empty, or only whitespace';
+exports.StringIsNotAValidGuid = 'Value is not a valid GUID';
 exports.ValueCannotBeNull = 'Value cannot be null or undefined';
 exports.InvalidCidr = 'CIDR is invalid';
 exports.InvalidIpAddress = 'IP Address is invalid';
+exports.resources = {
+    SubresourceNameShouldNotBeSpecifiedForTopLevelResourceType: 'SubresourceName should not be specified for top-level resource type'
+}
 exports.routeTable = {
     routes: {
         NextHopIpAddressCannotBePresent: 'nextHopIpAddress cannot be present for specified nextHopType',
