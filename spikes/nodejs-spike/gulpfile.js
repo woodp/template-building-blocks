896dce4af2511b7ea98a3ae5182b4c8dca7d0b7d
Andrew Oakley <anoakley@microsoft.com>
2017-05-05 17:45:37 -0700

    Add code coverage

--- spikes/nodejs-spike/gulpfile.js
+++ spikes/nodejs-spike/gulpfile.js
@@ -2,14 +2,29 @@
 
 let gulp = require('gulp');
 let jasmine = require('gulp-jasmine');
-let nyc = require('nyc');
+//let nyc = require('nyc');
+let istanbul = require('gulp-istanbul');
 
 // Workaround to make sure gulp exits on successful test run.
 gulp.doneCallback = (err) => {
     //process.exit(err ? 1 : 0);
     process.kill(process.pid);
 }
+
+gulp.task('pre-test', () =>{
+    return gulp.src(['core/**/*.js'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+});
+
 // Run tests
+gulp.task('run-tests-with-coverage', ['pre-test'], () => {
+    return gulp.src('spec/**/*.js')
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports());
+        //.pipe(nyc({}));
+});
+
 gulp.task('run-tests', () => {
     return gulp.src('spec/**/*.js')
         .pipe(jasmine());
