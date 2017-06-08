e99abaff91f86f14c698dcb1e655b6d549180373
RohitSharma-pnp <roshar@microsoft.com>
2017-06-08 13:36:20 -0700

    added validations for extensions building block

--- spikes/nodejs-spike/core/virtualMachineExtensionsSettings.js
+++ spikes/nodejs-spike/core/virtualMachineExtensionsSettings.js
@@ -8,14 +8,50 @@ function merge(settings) {
     return settings;
 }
 
+let vmExtensionValidations = {
+    vms: (value, parent) => {
+        if (_.isNil(value) || !_.isArray(value) || value.length === 0) {
+            return {
+                result: false,
+                message: 'Value (Array) cannot be null, undefined or empty'
+            };
+        } else {
+            return {
+                result: true
+            }
+        }
+    },
+    extensions: (value, parent) => {
+        if (_.isNil(value) || !_.isArray(value) || value.length === 0) {
+            return {
+                result: false,
+                message: 'Value (Array) cannot be null, undefined or empty'
+            };
+        }
+        let extensionValidations = {
+            name: v.validationUtilities.isNotNullOrWhitespace,
+            publisher: v.validationUtilities.isNotNullOrWhitespace,
+            type: v.validationUtilities.isNotNullOrWhitespace,
+            typeHandlerVersion: v.validationUtilities.isNotNullOrWhitespace,
+            autoUpgradeMinorVersion: v.validationUtilities.isBoolean,
+            settings: v.validationUtilities.isValidJsonObject,
+            protectedSettings: v.validationUtilities.isValidJsonObject
+        }
+         return {
+            validations: extensionValidations
+        };
+    },
+};
+
 function validate(settings) {
-    return {
-        result: true
-    };
+    return v.validate({
+        settings: settings,
+        validations: vmExtensionValidations
+    });
 }
 
 function process(param, buildingBlockSettings) {
-    let accumulator = {extensions: []};
+    let accumulator = { extensions: [] };
     param.forEach((value) => {
         value.extensions.forEach((ext) => {
             let setting = {
@@ -26,7 +62,7 @@ function process(param, buildingBlockSettings) {
             if (ext.protectedSettings.hasOwnProperty('reference') && ext.protectedSettings.reference.hasOwnProperty('keyVault')) {
                 setting.extensionProtectedSettings = ext.protectedSettings;
             } else {
-                setting.extensionProtectedSettings = {value: JSON.stringify(ext.protectedSettings)};
+                setting.extensionProtectedSettings = { value: JSON.stringify(ext.protectedSettings) };
             }
             let extension = _.cloneDeep(ext);
             delete extension.protectedSettings;
