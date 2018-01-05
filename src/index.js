d65a226dcd66d33f0d5a9325cc1dee0cc0352db5
Andrew Oakley <anoakley@microsoft.com>
2018-01-05 12:14:25 -0800

    Upgrade version to 2.1.2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const az = require('./azCLI');
 const semver = require('semver');
 
 
-const AZBB_VERSION = '2.1.1';
+const AZBB_VERSION = '2.1.2';
 
 let getDefaultOptions = () => {
     let defaultOptions = {
